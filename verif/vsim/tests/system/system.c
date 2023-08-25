0b82bdac55537ba21802da5308e33792658bc2cf
tonyho <TonyHo@via.com.tw>
2023-08-25 17:39:31 +0800

    check SYSTEM_test113 ok

--- verif/vsim/tests/system/system.c
+++ verif/vsim/tests/system/system.c
@@ -39,8 +39,8 @@
 //#define SYSTEM_test111 1
 //#define SYSTEM_test112 1
 //#define SYSTEM_test103 1
-#define SYSTEM_test104 1
-//#define SYSTEM_test113 1
+//#define SYSTEM_test104 1
+#define SYSTEM_test113 1
 //#define SYSTEM_test114 1
 // --------------------------------------------------------
 
