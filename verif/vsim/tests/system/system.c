c4f26756d560a73616ae75807e3970edda2b5870
tonyho <TonyHo@via.com.tw>
2023-08-25 16:52:54 +0800

    to workaround cpu do not issue wishbone write to mailbox by read it to flush last write

--- verif/vsim/tests/system/system.c
+++ verif/vsim/tests/system/system.c
@@ -187,7 +187,8 @@ void main()
     //Step 1. read io_serdes_base + 0
     value = *(volatile uint32_t*)(io_serdes_base + 0);
     *(volatile uint32_t*)(aa_base + AA_MailBox_Reg_Offset + 0 )= value;        //write to AA_MailBox_Reg_Offset + 0
-    value = *(volatile uint32_t*)(io_serdes_base + 0);
+    value = *(volatile uint32_t*)(aa_base + AA_MailBox_Reg_Offset + 0 )= value;        //read it to flush last write
+
     #endif  //SYSTEM_test112 
     
 	
